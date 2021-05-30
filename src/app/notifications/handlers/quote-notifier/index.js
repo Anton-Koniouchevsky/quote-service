d9ae0ab4b40d9970103f615638ed78371284476e
Anton Koniouchevski <Anton_Koniouchevski@epam.com>
2021-05-30 19:56:23 +0300

    feat: add email notification

--- src/app/notifications/handlers/quote-notifier/index.js
+++ src/app/notifications/handlers/quote-notifier/index.js
@@ -1,18 +1,72 @@
+const { SES } = require('aws-sdk');
+
+const sendEmail = async ({ email, quote }) => {
+  console.log('quote-service', 'sendEmail', email, quote);
+
+  const sesClient = new SES({ region: 'eu-west-1' });
+
+  const emailParams = {
+    Destination: {
+      ToAddresses: [email],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: `<p>${quote.text}</p><h1>${quote.author}<h1>`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Daily Quote',
+      },
+    },
+    Source: 'anton.koniouchevsky@gmail.com',
+  };
+
+  console.log('quote-service', 'sendEmail', emailParams);
+
+  return sesClient.sendEmail(emailParams).promise();
+};
+
+const sendSMS = async ({ phone, quote }) => {
+  console.log('quote-service', 'sendSMS', phone, quote);
+};
+
 exports.handler = async (event) => {
+  let statusCode = 200;
+  const response = {};
   console.log('quote-service', event);
 
-  const response = {
-    result: 'Hello World',
-  };
+  try {
+    const body = JSON.parse(event.body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  };
+    if (!body || !body.quote) {
+      throw new Error('no quote provided');
+    }
+
+    if (body.email) {
+      await sendEmail(body);
+    } else if (body.phone) {
+      await sendSMS(body);
+    } else {
+      throw new Error('unknown notification method');
+    }
+  } catch (err) {
+    statusCode = 404;
+    response.error = err.message;
+    console.log('quote-service', err);
+  } finally {
+    // eslint-disable-next-line no-unsafe-finally
+    return {
+      statusCode,
+      body: JSON.stringify(response),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    };
+  }
 };
